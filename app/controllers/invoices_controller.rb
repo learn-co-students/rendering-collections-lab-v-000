f464362d7f8699688643a4d764d22218db88c76d
johnsontaiwo <josseytee@gmail.com>
2016-11-08 09:36:55 -0500

    Done.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,5 +3,15 @@ class InvoicesController < ApplicationController
   def index
     @invoices = Invoice.all
   end
+  
+  def new
+    @invoice = Invoice.new
+  end
+
+  def show
+    binding.pry
+   @invoice = Invoice.find(params[:id])
+  
+  end
 
 end
