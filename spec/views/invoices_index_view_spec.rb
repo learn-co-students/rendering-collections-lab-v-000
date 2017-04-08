1791fe98b11d802a627b7f5c680a63ef5bb139e0
Scott Bewick <bewick.scott@gmail.com>
2017-04-07 19:11:25 -0500

    Done.

--- spec/views/invoices_index_view_spec.rb
+++ spec/views/invoices_index_view_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+ require "rails_helper"
 
 RSpec.describe "invoices_index_view" do
   # let(:classroom) { Classroom.create(course_name: 'Math', semester: "Spring #{Time.now.year}") }
