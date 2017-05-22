8bb1a20dea6c9922692c5ea1ab22d136335818dc
Logan McAnsh <logan@mcan.sh>
2017-05-22 18:26:57 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20160125172450) do
 
   create_table "customers", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,22 +24,22 @@ ActiveRecord::Schema.define(version: 20160125172450) do
   end
 
   create_table "ordered_products", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "order_id"
+    t.integer "product_id"
+    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "customer_id"
-    t.integer  "invoice_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "customer_id"
+    t.integer "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "price"
+    t.string "name"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
